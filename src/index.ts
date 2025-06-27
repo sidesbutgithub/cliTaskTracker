c8ea87eaebcccd77c9ab405d2e8ff3d2cc38048d
sidesbutgithub <SidneyWai3@gmail.com>
2025-06-27 17:31:55 -0400

    cleaned up index.ts, added update for taskMethods, todo: add suport for creating custom attributes

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,23 @@
 #!/usr/bin/env node 
 
-//ue synchronous file handling as this is a CLI tool
-
-//const { argv } = require('node:process');
+//use synchronous file handling as this is a CLI tool
 import {argv} from "node:process";
 import fs from "fs";
-
-//import {task, addTask} from './taskMethods';
-import {task, addTask, deleteTask} from './taskMethods.js';
-
+import {task, addTask, deleteTask, updateTask} from './taskMethods.js';
 const tasksList = "taskList.json";
-
-/*
-interface task {
-  desc: string,
-  status: "done" | "todo" | "in-progress",
-  createdAt?: string,
-  lastUpdate?: object,
-  dueDate?: null | string,
-}*/
-/*
-const testTask: task = {
-  desc : "testDesc",
-  status: "todo",
-
-}
-
-const testTask2: task = {
-  desc : "testDesc",
-  status: "todo"
-  
-}*/
-
 const tasks = fs.existsSync(tasksList) ? JSON.parse(fs.readFileSync(tasksList, { encoding: 'utf8' })) : {};
-
 
 switch (argv[2]){
   case "add":
     //argv[0] argv[1] add <taskname> <taskdesc> <duedate>
-    addTask(tasks, argv[3], argv[4], argv[5]);
+    addTask(tasks, argv[3], argv[4]);
     break;
   case "delete":
     console.log("called delete");
     deleteTask(tasks, argv[3]);
     break;
   case "update":
+    updateTask(tasks, argv[3], argv[4], argv[5]);
     console.log("called update");
     break;
   case "list":
--- src/taskMethods.ts
+++ src/taskMethods.ts
@@ -1,38 +1,84 @@
 interface task {
   desc: string,
-  status: "done" | "todo" | "in-progress",
+  taskStatus: "done" | "todo" | "in-progress",
   createdAt: string,
   lastUpdate: object,
-  dueDate: null | string,
 }
 
-function addTask(database: object, taskName:string, taskDesc:string|undefined, due: string):void{
+function addTask(database: object, taskName:string, taskDesc:string|undefined): void{
   if (taskName in database){
     console.log("task name already in use");
     return;
   }
+
   if (taskDesc===undefined){
     taskDesc = taskName;
   }
 
   const newTask = {
     desc: taskDesc,
-    status: "todo",
+    taskStatus: "todo",
     createdAt: Date(),
-    lastUpdate: {created: Date()},
-    dueDate: due
+    lastUpdate: {"created": Date()}
   }
   // @ts-ignore
   database[taskName] = newTask;
+  return;
 }
 
-function deleteTask(database: object, taskName:string):void{
+function deleteTask(database: object, taskName:string): void{
   if (!(taskName in database)){
     console.log("task name not in use");
     return;
   }
   // @ts-ignore
   delete database[taskName];
+  return;
 }
 
-export {task, addTask, deleteTask}
+function updateTask(database: object, taskName: string, attributeName: string, newVal: string): void{
+  if (!(taskName in database)){
+    console.log("task name not in use");
+    return;
+  }
+  // @ts-ignore
+  let currTask: task = database[taskName];
+  switch (attributeName){
+    case "createdAt":
+    case "lastUpdate":
+      console.log("unable to change target attribute of task");
+      return;
+    case "name":
+      if (newVal in database){
+        console.log("task name already in use");
+        return;
+      }
+      // @ts-ignore
+      database[newVal] = database[taskName];
+      // @ts-ignore
+      delete database[taskName];
+      break;
+    case "taskStatus":
+      if (!(newVal==="done" || newVal==="todo" || newVal==="in-progress")){
+        console.log("invalid value for taskStatus");
+        return;
+      }
+    case "desc":
+      // @ts-ignore
+      const updateLog = `update ${taskName}: ${currTask[attributeName]} -> ${newVal}`;
+      // @ts-ignore
+      currTask[attributeName] = newVal;
+      const newUpdate = {};
+      // @ts-ignore
+      newUpdate[updateLog] = Date();
+      currTask.lastUpdate = newUpdate;
+      break;
+    default:
+      console.log("attribute name not in use");
+      return;
+  }
+
+
+}
+
+export {task, addTask, deleteTask, updateTask}
