8ee5c9ecae21c8b7dc6fb2eab712558795f0cc5b
sidesbutgithub <SidneyWai3@gmail.com>
2025-06-27 07:06:32 -0400

    figured out build and run and link

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@
 import {argv} from "node:process";
 
 // print process.argv
+/*
 argv.forEach((val: string, index: number) => {
   console.log(`${index}: ${val}`);
   console.log(`${typeof index}: ${typeof val}`);
-});
+});*/
+console.log("change")
+console.log("change2")
