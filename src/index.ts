bde1cfe1640a84c193b89a7159016ee6c047e1c2
sidesbutgithub <SidneyWai3@gmail.com>
2025-06-27 07:53:15 -0400

    added task file json creation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 #!/usr/bin/env node 
+
+//ue synchronous file handling as this is a CLI tool
+
 //const { argv } = require('node:process');
 import {argv} from "node:process";
+import fs from "fs";
 
-// print process.argv
+let tasksList = "taskList.json";
+
+if (!fs.existsSync(tasksList)){
+  fs.writeFileSync(tasksList, "");
+}
+
+let testData = {"name": argv[2]};
+
+fs.writeFileSync(tasksList, JSON.stringify(testData));
 /*
 argv.forEach((val: string, index: number) => {
   console.log(`${index}: ${val}`);
   console.log(`${typeof index}: ${typeof val}`);
-});*/
-console.log("change")
-console.log("change2")
+})*/
+
+
+
+
+console.log(argv[2])
